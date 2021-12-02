00b9aae848941d54ed6eb47171280b41bf1fbf26
study <study@Satyajits-MacBook-Pro.local>
2021-12-02 18:41:38 +0530

    adding desc to codes

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-cd ~/Downloads/
-host=ec2-3-91-180-109.compute-1.amazonaws.com
+cd ~/Downloads/ # switching to my dir wher i have stored the key for my instance.
+host=ec2-3-91-180-109.compute-1.amazonaws.com # this is dynamic, changes all the time if not static
 username="ubuntu@"
 echo $username
 for i in $host; do 
 ssh -i "aws_key.pem" $username$i << EOF
-  sudo apt-get update;
-  sudo apt install docker.io;
-  sudo apt install docker;
-  sudo systemctl start docker;
-  sudo docker run -p 8080:8080 jenkins/jenkins:lts-jdk11 #exposing the port 8080 for jenkins. ref https://github.com/jenkinsci/docker/blob/master/README.md
+  sudo apt-get update; # updating the the ubunut image
+  sudo apt install docker.io; #installing docker
+  sudo apt install docker; 
+  sudo systemctl start docker; #starting docker 
+  sudo docker run -p 8080:8080 jenkins/jenkins:lts-jdk11 # exposing the port 8080 for jenkins. ref https://github.com/jenkinsci/docker/blob/master/README.md
 EOF
 done
 #sudo apt-get install docker -y 
