37bc5e15b8b38633116e978e840861cbe4acbef1
study <study@Satyajits-MacBook-Pro.local>
2021-12-02 18:29:30 +0530

    added egress rule to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,4 +26,17 @@ resource "aws_security_group" "main" {
 		security_groups = []
 		}	
 	]
+      egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
 }
