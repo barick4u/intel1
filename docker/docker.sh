609c23e2a40c45d1b7f167b7c078b5f7dbf90d16
study <study@Satyajits-MacBook-Pro.local>
2021-12-02 18:38:39 +0530

    adding steps to install docker and pull/run image of jenkins lts

--- docker/docker.sh
+++ docker/docker.sh
@@ -5,8 +5,11 @@ username="ubuntu@"
 echo $username
 for i in $host; do 
 ssh -i "aws_key.pem" $username$i << EOF
-  sudo apt-get update; 
-  mkdir /home/ubuntu/test3
+  sudo apt-get update;
+  sudo apt install docker.io;
+  sudo apt install docker;
+  sudo systemctl start docker;
+  sudo docker run -p 8080:8080 jenkins/jenkins:lts-jdk11 #exposing the port 8080 for jenkins. ref https://github.com/jenkinsci/docker/blob/master/README.md
 EOF
 done
 #sudo apt-get install docker -y 
